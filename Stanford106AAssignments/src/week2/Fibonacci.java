9865043528258f79851d619a591cf6d39b91eb34
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-02-13 22:01:12 -0500

    Update

--- Stanford106AAssignments/src/week2/Fibonacci.java
+++ Stanford106AAssignments/src/week2/Fibonacci.java
@@ -3,9 +3,9 @@ package week2;
 import acm.program.*;
 
 public class Fibonacci extends ConsoleProgram {
-
-	int term = 0;
+	//This will print the Fibonacci number
 	public void run() {
+		int term = 0;
 		while(term < 10) {
 			System.out.println(term);
 			term++;
