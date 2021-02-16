86d19bb48f493afb9b56461b06901a587f77bf6a
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-02-15 22:19:00 -0500

    Update

--- Stanford106AAssignments/src/week2/Fibonacci.java
+++ Stanford106AAssignments/src/week2/Fibonacci.java
@@ -1,14 +1,23 @@
 package week2;
 
 import acm.program.*;
+import acm.graphics.*;
 
 public class Fibonacci extends ConsoleProgram {
 	//This will print the Fibonacci number
+	
+	private static int MAX_TERM_VALUE = 10000;
+	
 	public void run() {
-		int term = 0;
-		while(term < 10) {
-			System.out.println(term);
-			term++;
+		println("This program lists the Fibonacci sequence.");
+		int num1 = 0;
+		int num2 = 1;
+		
+		while(num1 < MAX_TERM_VALUE) {
+			println(num1);
+			int sum = num1+num2;
+			num1 = num2;
+			num2 = sum;
 		}
 	}
 }
