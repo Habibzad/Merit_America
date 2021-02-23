1e5d642ce3e5471b2509d65f5bb0437a49739f8f
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-02-22 22:42:55 -0500

    Update

--- Stanford106AAssignments/src/week2/Fibonacci.java
+++ Stanford106AAssignments/src/week2/Fibonacci.java
@@ -2,11 +2,12 @@ package week2;
 
 import acm.program.*;
 import acm.graphics.*;
+//This program will print the Fibonacci number
 
 public class Fibonacci extends ConsoleProgram {
-	//This will print the Fibonacci number
 	
-	private static int MAX_TERM_VALUE = 10000;
+	//Maximum value
+	private static final int MAX_TERM_VALUE = 10000;
 	
 	public void run() {
 		println("This program lists the Fibonacci sequence.");
