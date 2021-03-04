e73a72538e45915dec1f566d33fa3848db3b9430
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-03-04 09:14:13 -0500

    update

--- Stanford106AAssignments/src/week4/WordCount.java
+++ Stanford106AAssignments/src/week4/WordCount.java
@@ -1,11 +1,60 @@
 package week4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import acm.program.ConsoleProgram;
+import acmx.export.java.io.FileReader;
 
 public class WordCount extends ConsoleProgram {
-	
+	int lineCounter = 0;
+	int wordCounter = 0;
+	int charCounter = 0;
+
 	public void run() {
-		println("test");
+
+		BufferedReader bufferedReader;
+		FileReader fileReader;
+		String line;
+		String fileName = "C:\\Users\\ahmad\\Desktop\\test.txt";
+
+		try {
+			fileReader = new FileReader(fileName);
+			bufferedReader = new BufferedReader(fileReader);
+			while (true) {
+				line = bufferedReader.readLine();
+				if (line == null)
+					break;
+				lineCounter++;
+				wordCounter += countWords(line);
+				charCounter += line.length();
+			}
+			bufferedReader.close();
+		} catch (IOException ex) {
+			println("An I/O exception has occurred");
+		}
+		println("File  = " + fileName);
+		println("Lines = " + lineCounter);
+		println("Words = " + wordCounter);
+		println("Chars = " + charCounter);
 	}
-	
+
+	// Words Count Method
+	private int countWords(String line) {
+		boolean inWord = false;
+		int totalWords = 0;
+		for (int i = 0; i < line.length(); i++) {
+			char ch = line.charAt(i);
+			if (Character.isLetterOrDigit(ch)) {
+				inWord = true;
+			} else {
+				if (inWord)
+					totalWords++;
+				inWord = false;
+			}
+		}
+		if (inWord)
+			totalWords++;
+		return totalWords;
+	}
+
 }
