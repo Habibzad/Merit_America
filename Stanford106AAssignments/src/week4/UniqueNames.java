2d5442c19d8cc87473acc93920f264f4c888c457
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-03-04 12:55:58 -0500

    Update unique names

--- Stanford106AAssignments/src/week4/UniqueNames.java
+++ Stanford106AAssignments/src/week4/UniqueNames.java
@@ -1,11 +1,32 @@
 package week4;
 
+import java.util.ArrayList;
+
 import acm.program.ConsoleProgram;
 
 public class UniqueNames extends ConsoleProgram {
-	
-	public void run() {
 
+	public void run() {
+		// Create list
+		ArrayList<String> namesList = new ArrayList<String>();
+		String name;
+
+		while (true) {
+			name = readLine("Enter a name: ");
+			if (name.equals("")) {
+				break;
+			}
+
+			if (!namesList.contains(name)) {
+				namesList.add(name);
+			}
+		}
+
+		println("Unique names list contains: ");
+
+		for (int i = 0; i < namesList.size(); i++) {
+			println(namesList.get(i));
+		}
 	}
-	
+
 }
